7048e7fb4ad0f91898eb37d89c4508dfb3e494e9
Deivid Rodrigues <deivid.arodrigues@gmail.com>
2025-01-01 18:39:49 -0300

    chore: cors treatment

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"github.com/deividr/zion-api/internal/controller"
+	controllers "github.com/deividr/zion-api/internal/controller"
 	"github.com/deividr/zion-api/internal/infra/database"
 	"github.com/deividr/zion-api/internal/infra/repository/postgres"
 	"github.com/deividr/zion-api/internal/usecase"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -27,6 +28,18 @@ func main() {
 
 	// Setup router
 	r := gin.Default()
+
+	// CORS configuration
+	corsConfig := cors.Config{
+		AllowOrigins:     []string{"*"}, // Altere para os domínios permitidos
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}
+
+	r.Use(cors.New(corsConfig))
+
 	r.GET("/products", productController.GetAll)
 	r.GET("/products/:id", productController.GetById)
 
