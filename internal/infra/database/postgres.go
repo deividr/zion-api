c3d56e491eb9e99705d3d13b98c658a04fd817cf
Deivid Rodrigues <deivid.arodrigues@gmail.com>
2025-03-30 18:39:37 -0300

    chore: better pool connection configs

--- internal/infra/database/postgres.go
+++ internal/infra/database/postgres.go
@@ -9,8 +9,18 @@ import (
 )
 
 func GetConnection() *pgxpool.Pool {
-	dbpool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	config, err := pgxpool.ParseConfig(os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to parse connection string: %v\n", err)
+		os.Exit(1)
+	}
 
+	// Configurações do pool de conexões
+	config.MaxConns = 10
+	config.MaxConnLifetime = 0
+	config.MaxConnIdleTime = 0
+
+	dbpool, err := pgxpool.NewWithConfig(context.Background(), config)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
 		os.Exit(1)
